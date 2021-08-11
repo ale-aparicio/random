a6bf189b025ab53df5704d0533d12f9d594eb9d8
ale-aparicio <ale.aparicio@live.com>
2021-08-11 14:38:02 +0000

    add

--- run.py
+++ run.py
@@ -16,17 +16,19 @@ def explore():
 
 
 @app.route("/media")
-def explore():
+def media():
     return render_template("media.html")
 
 
 @app.route("/theories")
-def explore():
+def theories():
     return render_template("theories.html")
 
 
+#remember to change debug = true to false
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True)
+
